56239bde7fd95a6013da802f989ca88b20d12341
Lasha Efremidze <efremidzel@hotmail.com>
2014-07-24 19:34:55 -0700

    Updated NotesViewController

--- STD/Controllers/STDNotesViewController.m
+++ STD/Controllers/STDNotesViewController.m
@@ -7,12 +7,11 @@
 //
 
 #import "STDNotesViewController.h"
+#import "UIViewController+BHTKeyboardNotifications.h"
 
 @interface STDNotesViewController () <UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextView *textView;
-
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomConstraint;
 
 @end
 
@@ -32,6 +31,8 @@
     
     self.textView.text = self.task.note.body;
     
+    [self registerForKeyboardNotifications];
+    
     [self.textView becomeFirstResponder];
 }
 
@@ -43,30 +44,6 @@
 }
 
 #pragma mark - UITextViewDelegate
-
-- (BOOL)textViewShouldBeginEditing:(UITextView *)textView;
-{
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-
-    return YES;
-}
-
-- (void)textViewDidBeginEditing:(UITextView *)textView
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
-}
-
-- (BOOL)textViewShouldEndEditing:(UITextView *)textView;
-{
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-    
-    return YES;
-}
-
-- (void)textViewDidEndEditing:(UITextView *)textView
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
-}
 
 - (void)textViewDidChange:(UITextView *)textView
 {
@@ -81,44 +58,44 @@
 - (void)showTextViewCaretPosition:(UITextView *)textView
 {
     CGRect caretRect = [textView caretRectForPosition:textView.selectedTextRange.end];
-    [textView scrollRectToVisible:caretRect animated:NO];
+    [textView scrollRectToVisible:caretRect animated:YES];
 }
 
 #pragma mark - Keyboard
+
+- (void)registerForKeyboardNotifications
+{
+    __weak typeof(self) weakSelf = self;
+    
+    [self setKeyboardWillShowAnimationBlock:^(CGRect keyboardFrame) {
+        typeof(self) self = weakSelf;
+        
+        [self keyboardFrameChanged:keyboardFrame];
+    }];
+    
+    [self setKeyboardWillHideAnimationBlock:^(CGRect keyboardFrame) {
+        typeof(self) self = weakSelf;
+        
+        [self keyboardFrameChanged:keyboardFrame];
+    }];
+}
 
 static CGFloat contentOffsetForBottom(CGRect keyboardFrame) {
     UIWindow *window = [[UIApplication sharedApplication] keyWindow];
     UIView *view = window.rootViewController.view;
     CGRect convertedRect = [view convertRect:keyboardFrame fromView:nil];
-    return CGRectIsNull(convertedRect) ? 0 : CGRectGetHeight(convertedRect);
+    CGFloat offset = CGRectGetHeight(view.frame) - CGRectGetMinY(convertedRect);
+    return CGRectIsNull(convertedRect) ? 0 : offset;
 }
 
-- (void)keyboardWillShow:(NSNotification *)notification
+- (void)keyboardFrameChanged:(CGRect)newFrame
 {
-    [self handleKeyboardNotification:notification];
-}
-
-- (void)keyboardWillHide:(NSNotification *)notification
-{
-    [self handleKeyboardNotification:notification];
-}
-
-- (void)handleKeyboardNotification:(NSNotification *)notification
-{
-    CGRect frameEnd;
-    [[[notification userInfo] valueForKey:UIKeyboardFrameEndUserInfoKey] getValue:&frameEnd];
+    if (CGRectIsNull(newFrame))
+        return;
     
-    double animationDuration;
-    [[[notification userInfo] valueForKey:UIKeyboardAnimationDurationUserInfoKey] getValue:&animationDuration];
-    
-    UIViewAnimationCurve animationCurve;
-    [[[notification userInfo] valueForKey:UIKeyboardAnimationCurveUserInfoKey] getValue:&animationCurve];
-    
-    self.bottomConstraint.constant = contentOffsetForBottom(frameEnd);
-    
-    [UIView animateWithDuration:animationDuration delay:0.0f options:((animationCurve << 16) | UIViewAnimationOptionBeginFromCurrentState) animations:^{
-        [self.view layoutIfNeeded];
-    } completion:nil];
+    UIEdgeInsets edgeInsets = self.textView.contentInset;
+    edgeInsets.bottom = MAX(0.0f, contentOffsetForBottom(newFrame));
+    self.textView.contentInset = edgeInsets;
 }
 
 @end
