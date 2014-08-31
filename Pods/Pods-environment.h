56ee70e82e67caa6c84811b3946e71a6a6ff8a84
Lasha Efremidze <efremidzel@hotmail.com>
2014-08-31 16:48:49 -0700

    Added SWTableViewCell Pod

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_MagicalRecord_Shorthand 2
 #define COCOAPODS_VERSION_PATCH_MagicalRecord_Shorthand 0
 
+// SWTableViewCell
+#define COCOAPODS_POD_AVAILABLE_SWTableViewCell
+#define COCOAPODS_VERSION_MAJOR_SWTableViewCell 0
+#define COCOAPODS_VERSION_MINOR_SWTableViewCell 3
+#define COCOAPODS_VERSION_PATCH_SWTableViewCell 3
+
 // SZTextView
 #define COCOAPODS_POD_AVAILABLE_SZTextView
 #define COCOAPODS_VERSION_MAJOR_SZTextView 1
