b3e7976e5677f138957714c4e4fa001855f44354
Lasha Efremidze <efremidzel@hotmail.com>
2014-10-05 19:07:34 -0700

    Added PureLayout

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -18,6 +18,12 @@
 #define COCOAPODS_VERSION_MINOR_MagicalRecord_Shorthand 2
 #define COCOAPODS_VERSION_PATCH_MagicalRecord_Shorthand 0
 
+// PureLayout
+#define COCOAPODS_POD_AVAILABLE_PureLayout
+#define COCOAPODS_VERSION_MAJOR_PureLayout 1
+#define COCOAPODS_VERSION_MINOR_PureLayout 1
+#define COCOAPODS_VERSION_PATCH_PureLayout 0
+
 // SZTextView
 #define COCOAPODS_POD_AVAILABLE_SZTextView
 #define COCOAPODS_VERSION_MAJOR_SZTextView 1
