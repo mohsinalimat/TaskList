0a364219ca8a099bff6fc879393f4a2fc8002e87
Lasha Efremidze <efremidzel@hotmail.com>
2015-01-29 11:13:33 -0800

    Updated Pods

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,42 +6,6 @@
 // project.
 
 
-// FontAwesomeKit
-#define COCOAPODS_POD_AVAILABLE_FontAwesomeKit
-#define COCOAPODS_VERSION_MAJOR_FontAwesomeKit 2
-#define COCOAPODS_VERSION_MINOR_FontAwesomeKit 1
-#define COCOAPODS_VERSION_PATCH_FontAwesomeKit 8
-
-// FontAwesomeKit/Core
-#define COCOAPODS_POD_AVAILABLE_FontAwesomeKit_Core
-#define COCOAPODS_VERSION_MAJOR_FontAwesomeKit_Core 2
-#define COCOAPODS_VERSION_MINOR_FontAwesomeKit_Core 1
-#define COCOAPODS_VERSION_PATCH_FontAwesomeKit_Core 8
-
-// FontAwesomeKit/FontAwesome
-#define COCOAPODS_POD_AVAILABLE_FontAwesomeKit_FontAwesome
-#define COCOAPODS_VERSION_MAJOR_FontAwesomeKit_FontAwesome 2
-#define COCOAPODS_VERSION_MINOR_FontAwesomeKit_FontAwesome 1
-#define COCOAPODS_VERSION_PATCH_FontAwesomeKit_FontAwesome 8
-
-// FontAwesomeKit/FoundationIcons
-#define COCOAPODS_POD_AVAILABLE_FontAwesomeKit_FoundationIcons
-#define COCOAPODS_VERSION_MAJOR_FontAwesomeKit_FoundationIcons 2
-#define COCOAPODS_VERSION_MINOR_FontAwesomeKit_FoundationIcons 1
-#define COCOAPODS_VERSION_PATCH_FontAwesomeKit_FoundationIcons 8
-
-// FontAwesomeKit/IonIcons
-#define COCOAPODS_POD_AVAILABLE_FontAwesomeKit_IonIcons
-#define COCOAPODS_VERSION_MAJOR_FontAwesomeKit_IonIcons 2
-#define COCOAPODS_VERSION_MINOR_FontAwesomeKit_IonIcons 1
-#define COCOAPODS_VERSION_PATCH_FontAwesomeKit_IonIcons 8
-
-// FontAwesomeKit/Zocial
-#define COCOAPODS_POD_AVAILABLE_FontAwesomeKit_Zocial
-#define COCOAPODS_VERSION_MAJOR_FontAwesomeKit_Zocial 2
-#define COCOAPODS_VERSION_MINOR_FontAwesomeKit_Zocial 1
-#define COCOAPODS_VERSION_PATCH_FontAwesomeKit_Zocial 8
-
 // JRSwizzle
 #define COCOAPODS_POD_AVAILABLE_JRSwizzle
 #define COCOAPODS_VERSION_MAJOR_JRSwizzle 1
@@ -77,10 +41,4 @@
 #define COCOAPODS_VERSION_MAJOR_iRate 1
 #define COCOAPODS_VERSION_MINOR_iRate 11
 #define COCOAPODS_VERSION_PATCH_iRate 3
-
-// pop
-#define COCOAPODS_POD_AVAILABLE_pop
-#define COCOAPODS_VERSION_MAJOR_pop 1
-#define COCOAPODS_VERSION_MINOR_pop 0
-#define COCOAPODS_VERSION_PATCH_pop 7
 
